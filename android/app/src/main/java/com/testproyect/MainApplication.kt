2b9e64d23595ba4f8648779f5247f69422076c39
Luis Henandez <luis.j.hernandez@despegar.com>
2024-07-24 17:38:03 -0300

    delete packages

--- android/app/src/main/java/com/testproyect/MainApplication.kt
+++ android/app/src/main/java/com/testproyect/MainApplication.kt
@@ -1,7 +1,6 @@
 package com.testproyect
 
 import android.app.Application
-import com.despegartest.WebViewPackage
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -17,11 +16,7 @@ class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-                add(WebViewPackage())
-            }
+            PackageList(this).packages
 
         override fun getJSMainModuleName(): String = "index"
 
--- modules/android/src/main/java/com/despegartest/WebViewSDK.kt
+++ modules/android/src/main/java/com/despegartest/WebViewSDK.kt
@@ -2,9 +2,9 @@ package com.despegartest
 
 import android.content.Intent
 import android.net.Uri
-//import com.despegar.whitelabel.webkit.presentation.DespegarWebviewKit
-//import com.despegar.whitelabel.webkit.presentation.enviroment.DespegarWebKitFeatures
-//import com.despegar.whitelabel.webkit.presentation.enviroment.DespegarWebviewKitConfiguration
+import com.despegar.whitelabel.webkit.presentation.DespegarWebviewKit
+import com.despegar.whitelabel.webkit.presentation.enviroment.DespegarWebKitFeatures
+import com.despegar.whitelabel.webkit.presentation.enviroment.DespegarWebviewKitConfiguration
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
@@ -16,12 +16,12 @@ class WebViewSDK(private val reactContext: ReactApplicationContext)
   @ReactMethod fun showWebView(url: String) {
     val activity = reactContext.currentActivity
 
-    /*val configuration = DespegarWebviewKitConfiguration(uri = Uri.parse(url))
+    val configuration = DespegarWebviewKitConfiguration(uri = Uri.parse(url))
     configuration.features = listOf(DespegarWebKitFeatures.CLOSE_BUTTON_ENABLE)
     val intent: Intent = DespegarWebviewKit.createWebviewIntent(configuration)
 
     activity?.let {
       it.startActivity(intent)
-    }*/
+    }
   }
 }
